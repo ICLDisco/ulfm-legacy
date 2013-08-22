521c48a5cb198fea7321c43a827e0d13e10ce927
George Bosilca <bosilca@users.noreply.github.com>
2013-08-22 03:59:03 +0200

    Simplify the logic of checking the request for errors.

--- ompi/request/req_ft.c
+++ ompi/request/req_ft.c
@@ -33,7 +33,8 @@
 #include "ompi/mca/coll/base/coll_tags.h"
 #include "ompi/mca/pml/base/pml_base_request.h"
 
-#define REQUEST_IS_COLLECTIVE(req) ( ((req)->req_tag <= MCA_COLL_BASE_TAG_MIN) && ((req)->req_tag >= MCA_COLL_BASE_TAG_MAX_PRE_AGREEMENT) )
+#define REQUEST_IS_COLLECTIVE(req) (((req)->req_tag <= MCA_COLL_BASE_TAG_MIN) && \
+                                    ((req)->req_tag >= MCA_COLL_BASE_TAG_MAX_PRE_AGREEMENT))
 
 /**************************
  * Support Routines
@@ -76,25 +77,20 @@ bool ompi_request_state_ok(ompi_request_t *req)
         (req->req_tag >= MPI_ANY_TAG || REQUEST_IS_COLLECTIVE(req) ) ) {
         /* Do not set req->req_status.MPI_SOURCE */
         req->req_status.MPI_ERROR  = MPI_ERR_REVOKED;
-        if( !req->req_complete ) {
-            ompi_request_cancel(req);
-            ompi_request_complete(req, false);
-        }
-        req->req_complete = true;
 
         opal_output_verbose(10, ompi_ftmpi_output_handle,
                             "%s ompi_request_state_ok: Communicator has been revoked!",
                             ORTE_NAME_PRINT(ORTE_PROC_MY_NAME));
-        return false;
+        goto return_with_error;
     }
     /*
      * Point-to-Point Check:
      * If the request is -not- part of a collective then it is point-to-point.
      * Only need to check the specific peer target/source.
      */
-    else if( req->req_tag >= 0 ||
-             MPI_ANY_TAG == req->req_tag ||
-             !REQUEST_IS_COLLECTIVE(req) ) {
+    if( req->req_tag >= 0 ||
+        MPI_ANY_TAG == req->req_tag ||
+        !REQUEST_IS_COLLECTIVE(req) ) {
         if( req->req_peer == MPI_PROC_NULL ) {
             return true;
         }
@@ -108,21 +104,15 @@ bool ompi_request_state_ok(ompi_request_t *req)
              * removed from the message queue. Keep the request active, so the
              * user can keep waiting on it.
              */
+            req->req_status.MPI_SOURCE = req->req_peer;
             if( MPI_ANY_SOURCE == req->req_peer ) {
-                /* Do not set req->req_status.MPI_SOURCE */
                 req->req_any_source_pending = true;
             }
             /*
              * Directed peer errors are always critical, and return in error.
              */
             else {
-                req->req_status.MPI_SOURCE = req->req_peer;
                 req->req_status.MPI_ERROR  = MPI_ERR_PROC_FAILED;
-                if( !req->req_complete ) {
-                    ompi_request_cancel(req);
-                    ompi_request_complete(req, false);
-                }
-                req->req_complete = true;
             }
 
             opal_output_verbose(10, ompi_ftmpi_output_handle,
@@ -131,32 +121,34 @@ bool ompi_request_state_ok(ompi_request_t *req)
                                 req->req_status.MPI_SOURCE,
                                 req->req_mpi_object.comm->c_contextid,
                                 (MPI_ERR_PROC_FAILED == req->req_status.MPI_ERROR ? "MPI_ERR_PROC_FAILED" : "MPI_ERR_PENDING") );
-            return false;
+            goto return_with_error;
         }
+        return true;
     }
     /*
      * Collectives Check:
      * If the request is part of a collective, then the whole communicator
      * must be ok to continue. If not then return first failed process
      */
-    else if( ompi_comm_force_error_on_collectives(req->req_mpi_object.comm) ) {
+    if( ompi_comm_force_error_on_collectives(req->req_mpi_object.comm) ) {
         /* Return the last process known to have failed, may not have been the
          * first to cause the collectives to be disabled.
          */
         req->req_status.MPI_SOURCE = req->req_peer;
         req->req_status.MPI_ERROR  = MPI_ERR_PROC_FAILED;
-        if( !req->req_complete ) {
-            ompi_request_cancel(req);
-            ompi_request_complete(req, false);
-        }
-        req->req_complete = true;
 
         opal_output_verbose(10, ompi_ftmpi_output_handle,
                             "%s ompi_request_state_ok: Request is part of a collective, and some process died. (rank %3d)",
                             ORTE_NAME_PRINT(ORTE_PROC_MY_NAME), req->req_status.MPI_SOURCE );
-
-        return false;
+        goto return_with_error;
     }
 
     return true;
+
+ return_with_error:
+    if( !req->req_complete && (MPI_ANY_SOURCE != req->req_peer)) {
+        ompi_request_cancel(req);
+        ompi_request_complete(req, false);
+    }
+    return (MPI_SUCCESS == req->req_status.MPI_ERROR);
 }
