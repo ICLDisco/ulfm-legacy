2de40435aa9258663ebf13445417f75be7ecf0fc
George Bosilca <bosilca@eecs.utk.edu>
2014-01-31 16:15:50 +0100

    Allow cancel of send requests if the destination process is dead. Only mark the request as MPI_COMPLETE (and not PML_COMPLETE) to prevent the system from releasing the associated resources as we might have pending fragments.

--- ompi/mca/pml/ob1/pml_ob1_sendreq.c
+++ ompi/mca/pml/ob1/pml_ob1_sendreq.c
@@ -128,6 +128,34 @@ static int mca_pml_ob1_send_request_free(struct ompi_request_t** request)
 
 static int mca_pml_ob1_send_request_cancel(struct ompi_request_t* request, int complete)
 {
+#if OPAL_ENABLE_FT_MPI
+    if( true == request->req_complete ) { /* way to late to cancel this one */
+        return OMPI_SUCCESS;
+    }
+    /* If FT is enabled then we allow canceling requests with a dead process.
+     * However, we should not release the request as we don't know if there are
+     * no pending fragments. This generates a memory leak as the send requests
+     * will never be recovered. */
+    if( !ompi_comm_is_proc_active(request->req_mpi_object.comm, request->req_peer,
+                                  OMPI_COMM_IS_INTRA(request->req_mpi_object.comm)) ) {
+        mca_pml_ob1_send_request_t* pml_req = (mca_pml_ob1_send_request_t*)request;
+        /**
+         * As now the PML is done with this request we have to force the pml_complete
+         * to true. Otherwise, the request will never be freed.
+         */
+        pml_req->req_send.req_base.req_pml_complete = true;
+
+        OPAL_THREAD_LOCK(&ompi_request_lock);
+        request->req_status._cancelled = true;
+        /* This macro will set the req_complete to true so the MPI Test/Wait* functions
+         * on this request will be able to complete. As the status is marked as
+         * cancelled the cancel state will be detected.
+         */
+        MCA_PML_OB1_SEND_REQUEST_MPI_COMPLETE(pml_req, 0);
+        OPAL_THREAD_UNLOCK(&ompi_request_lock);
+        return OMPI_SUCCESS;
+    }
+#endif  /* OPAL_ENABLE_FT_MPI */
     /* we dont cancel send requests by now */
     return OMPI_SUCCESS;
 }
