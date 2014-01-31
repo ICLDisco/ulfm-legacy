e884fee4b1e7dd9a2d9dfb8694b6aacdfadc7a34
Aurelien Bouteiller <bouteill@icl.utk.edu>
2014-01-31 17:41:22 -0500

    Uniformization with other ft tests

--- test/mpi/ft/err_returns.c
+++ test/mpi/ft/err_returns.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 The University of Tennessee and The University
+ * Copyright (c) 2014      The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  *
@@ -17,28 +17,31 @@
 #include <mpi.h>
 
 int main( int argc, char* argv[] ) { 
-    int np, rank;
+    int np, rank, victim;
     int rc;
-    double start, tb, mtb, Mtb, tbf1, Mtbf1, mtbf1, tbf2, Mtbf2, mtbf2;
+    double start, tff, mtff, Mtff, tf1, Mtf1, mtf1, tf2, Mtf2, mtf2;
     int st;
     char estr[MPI_MAX_ERROR_STRING]=""; int strl;
-    MPI_Comm scomm;
+    MPI_Comm fcomm, scomm;
     int verbose=0;
     
     MPI_Init( &argc, &argv );
     
     if( !strcmp( argv[argc-1], "-v" ) ) verbose=1;
 
-    MPI_Comm_size( MPI_COMM_WORLD, &np );
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+    MPI_Comm_dup( MPI_COMM_WORLD, &fcomm );
+    MPI_Comm_size( fcomm, &np );
+    MPI_Comm_rank( fcomm, &rank );
+    victim = (rank == np-1)? 1 : 0;
+    MPI_Comm_split( fcomm, victim, rank, &scomm );
 
-    MPI_Comm_split( MPI_COMM_WORLD, (rank==np-1)?1:0, rank, &scomm );
     start=MPI_Wtime();
-    MPI_Barrier( MPI_COMM_WORLD );
-    tb=MPI_Wtime()-start;
-    MPI_Comm_set_errhandler( MPI_COMM_WORLD, MPI_ERRORS_RETURN );
+    MPI_Barrier( fcomm );
+    tff=MPI_Wtime()-start;
 
-    if( rank == np-1 ) {
+    MPI_Comm_set_errhandler( fcomm, MPI_ERRORS_RETURN );
+
+    if( victim ) {
         printf( "Rank %04d: committing suicide\n", rank );
         raise( SIGKILL );
         while(1); /* wait for the signal */
@@ -46,14 +49,14 @@ int main( int argc, char* argv[] ) {
     
     if(verbose) printf( "Rank %04d: entering Barrier\n", rank );
     start=MPI_Wtime();
-    rc = MPI_Barrier( MPI_COMM_WORLD );
-    tbf1=MPI_Wtime()-start;
+    rc = MPI_Barrier( fcomm );
+    tf1=MPI_Wtime()-start;
     if(verbose) { 
         MPI_Error_string( rc, estr, &strl );
-        printf( "Rank %04d: Barrier1 completed (rc=%s) duration %g (s)\n", rank, estr, tbf1 );
+        printf( "Rank %04d: Barrier1 completed (rc=%s) duration %g (s)\n", rank, estr, tf1 );
     }
-    if( rc != MPI_ERR_PROC_FAILED ) MPI_Abort( scomm, rc );
-    st = ceil(10*fmax(1., tb));
+    if( rc != MPI_ERR_PROC_FAILED ) MPI_Abort( MPI_COMM_WORLD, rc );
+    st = ceil(10*fmax(1., tff));
 
     /* operation on scomm should not raise an error, only procs 
      * not appearing in scomm are dead */
@@ -63,27 +66,30 @@ int main( int argc, char* argv[] ) {
 
     if(verbose) printf( "Rank %04d: entering Barrier\n", rank );
     start=MPI_Wtime();
-    rc = MPI_Barrier( MPI_COMM_WORLD );
-    tbf2=MPI_Wtime()-start;
+    rc = MPI_Barrier( fcomm );
+    tf2=MPI_Wtime()-start;
     if(verbose) { 
         MPI_Error_string( rc, estr, &strl );
-        printf( "Rank %04d: Barrier2 completed (rc=%s) duration %g (s)\n", rank, estr, tbf2 );
+        printf( "Rank %04d: Barrier2 completed (rc=%s) duration %g (s)\n", rank, estr, tf2 );
     }
-    if( rc != MPI_ERR_PROC_FAILED ) MPI_Abort( scomm, rc );
+    if( rc != MPI_ERR_PROC_FAILED ) MPI_Abort( MPI_COMM_WORLD, rc );
 
-    MPI_Reduce( &tb, &mtb, 1, MPI_DOUBLE, MPI_MIN, 0, scomm );
-    MPI_Reduce( &tb, &Mtb, 1, MPI_DOUBLE, MPI_MAX, 0, scomm );
-    MPI_Reduce( &tbf1, &mtbf1, 1, MPI_DOUBLE, MPI_MIN, 0, scomm );
-    MPI_Reduce( &tbf1, &Mtbf1, 1, MPI_DOUBLE, MPI_MAX, 0, scomm );
-    MPI_Reduce( &tbf2, &mtbf2, 1, MPI_DOUBLE, MPI_MIN, 0, scomm );
-    MPI_Reduce( &tbf2, &Mtbf2, 1, MPI_DOUBLE, MPI_MAX, 0, scomm );
+    MPI_Reduce( &tff, &mtff, 1, MPI_DOUBLE, MPI_MIN, 0, scomm );
+    MPI_Reduce( &tff, &Mtff, 1, MPI_DOUBLE, MPI_MAX, 0, scomm );
+    MPI_Reduce( &tf1, &mtf1, 1, MPI_DOUBLE, MPI_MIN, 0, scomm );
+    MPI_Reduce( &tf1, &Mtf1, 1, MPI_DOUBLE, MPI_MAX, 0, scomm );
+    MPI_Reduce( &tf2, &mtf2, 1, MPI_DOUBLE, MPI_MIN, 0, scomm );
+    MPI_Reduce( &tf2, &Mtf2, 1, MPI_DOUBLE, MPI_MAX, 0, scomm );
 
     if( 0 == rank ) printf( 
         "## Timings ########### Min         ### Max         ##\n"
         "Barrier (no fault)  # %13.5e # %13.5e\n"
         "Barrier (new fault) # %13.5e # %13.5e\n"
         "Barrier (old fault) # %13.5e # %13.5e\n",
-        mtb, Mtb, mtbf1, Mtbf1, mtbf2, Mtbf2 );
+        mtff, Mtff, mtf1, Mtf1, mtf2, Mtf2 );
+
+    MPI_Comm_free( &fcomm );
+    MPI_Comm_free( &scomm );
 
     MPI_Finalize();
     return EXIT_SUCCESS;
